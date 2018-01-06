040baf2f9a0d58ca018d7635a1188f5082e095d9
asyeif <32222060+asyeif@users.noreply.github.com>
2018-01-06 16:07:35 +0700

    Update call.js

--- call.js
+++ call.js
@@ -23,7 +23,9 @@ var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/p/pos1.html"}
 if(x=="2"){xxx="/p/pos2.html"}
-if(x=="3"){xxx="/p/pos3.html"}a_to_ve[i].href="http://s.yucek.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="3"){xxx="/p/pos3.html"}
+if(x=="4"){xxx="/p/pos4.html"}
+if(x=="5"){xxx="/p/pos5.html"}a_to_ve[i].href="http://s.yucek.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
